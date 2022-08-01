d601dd6713e6528417d90827661d60dc2c00441b
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-08-01 16:35:28 +0300

    Simplify move and prettify script.js

--- script.js
+++ script.js
@@ -9,367 +9,305 @@ const playBtn = document.getElementById("play");
 let snakeLen = 4;
 let tag = 0;
 
-
 // =========== CREATE GAME GRID ===========  //
 
 const createGame = (rows) => {
-    for (let i = 0; i < rows; i++) {
-        let box = templateBox.content.cloneNode(true);
-        gameBoard.append(box);
-    } 
-}
+  for (let i = 0; i < rows; i++) {
+    let box = templateBox.content.cloneNode(true);
+    gameBoard.append(box);
+  }
+};
 
 createGame(398);
 
 // =========== SNAKE MOVEMENT ===========  //
 const snakeTrack = {
-    // Head 
-    row: 1,
-    col: 4,
-    body: [
-        // Read [row, col]
-        // Head
-        [1, 1],
-        // Body
-        [1, 1],
-        [1, 1],
-        // Tail
-        [1, 1]
-    ], 
-    
-    direction: "none",
-}
+  // Head
+  row: 1,
+  col: 4,
+  body: [
+    // Read [row, col]
+    // Head
+    [1, 1],
+    // Body
+    [1, 1],
+    [1, 1],
+    // Tail
+    [1, 1],
+  ],
+
+  direction: "none",
+};
 
 // SNAKE TRACKING
 const trackSnake = () => {
-    head.classList.remove("head");
+  head.classList.remove("head");
 
-    snake = document.querySelectorAll(".snake-segment");
-    console.log(snake, tag);
-    
-    snake[tag].classList.add("head");
-    head = document.querySelector(".head");
+  snake = document.querySelectorAll(".snake-segment");
+  console.log(snake, tag);
 
-    head.style.gridRow = snakeTrack.row;
-    head.style.gridColumn = snakeTrack.col;
+  snake[tag].classList.add("head");
+  head = document.querySelector(".head");
 
-    if (tag == (snakeLen-1)) tag = 0;
-    else tag++;
-}
+  head.style.gridRow = snakeTrack.row;
+  head.style.gridColumn = snakeTrack.col;
+
+  if (tag == snakeLen - 1) tag = 0;
+  else tag++;
+};
 
 const move = () => {
-    let newTail = [];
-    switch (snakeTrack.direction) {
-        case "r":
-        // Move right
-        // Take first element and move it forward
-            
-            let nextRight = [];
-            nextRight.push(snakeTrack.body[0][0], snakeTrack.body[0][1]); // take body's first element = head
-            nextRight[1]++; // col++
-            
-            newTail = eatFood(nextRight);
-            
-            if (newTail) {
-                // move forward
-                snakeTrack.body.unshift(nextRight); // add new head coordinates to front of body
-                eatGrow();
-            }
-            else {
-                // move as normal
-                snakeTrack.body.unshift(nextRight); // add new head coordinates to front of body
-                snakeTrack.body.pop(); // remove last element
-            }
-            
-        // Update snakeTrack.col from body's first element
-            snakeTrack.col = snakeTrack.body[0][1];
+  let newTail = [];
+  let newHead = [...snakeTrack.body[0]];
+  switch (snakeTrack.direction) {
+    case "r":
+      // Move right
+      // Take first element and move it forward
+      newHead[1]++; // col++
+      break;
 
+    case "l":
+      // Move left
+      // Take first element and move it forward
+      newHead[1]--; // col--
 
-            trackSnake();
+      break;
+    case "u":
+      // Move up
+      // Take first element and move it forward
+      newHead[0]--; // row--
 
-            break;
-        
-        case "l":
-        // Move left
-        // Take first element and move it forward
-            let nextLeft = [];
-            nextLeft.push(snakeTrack.body[0][0], snakeTrack.body[0][1]); // take body's first element = head
-            nextLeft[1]--; // col--
+      break;
+    case "d":
+      // Move down
+      // Take first element and move it forward
+      newHead[0]++; // row++
 
-            newTail = eatFood(nextLeft);            
-            if (newTail) {
-                // move forward
-                snakeTrack.body.unshift(nextLeft); // add new head coordinates to front of body
-                eatGrow();
-            }
-            else {
-                // move as normal
-                snakeTrack.body.unshift(nextLeft); // add new head coordinates to front of body
-                snakeTrack.body.pop(); // remove last element
-            }
-            
-        // Update snakeTrack.col from body's first element
-            snakeTrack.col = snakeTrack.body[0][1];
+      break;
 
-            trackSnake();
-            break;
-        case "u":
-        // Move up
-            // Take first element and move it forward
-            let nextUp = [];
-            nextUp.push(snakeTrack.body[0][0], snakeTrack.body[0][1]); // take body's first element = head
-            nextUp[0]--; // row--
-            
-            newTail = eatFood(nextUp);
+    case "pause":
+      // Pause direction
 
-            if (newTail) {
-                // move forward
-                snakeTrack.body.unshift(nextUp); // add new head coordinates to front of body
-                eatGrow();
-            }
-            else {
-                // move as normal
-                snakeTrack.body.unshift(nextUp); // add new head coordinates to front of body
-                snakeTrack.body.pop(); // remove last element
-            }
-            
-        // Update snakeTrack.col from body's first element
-            snakeTrack.row = snakeTrack.body[0][0];
-                        
-            trackSnake();
-            break;
-        case "d":
-        // Move down
-            // Take first element and move it forward
-            
-            let nextDown = [];
-            nextDown.push(snakeTrack.body[0][0], snakeTrack.body[0][1]); // take body's first element = head
-            nextDown[0]++; // row++
-            
-            newTail = eatFood(nextDown);
+      return;
 
-            if (newTail) {
-                // move forward
-                snakeTrack.body.unshift(nextDown); // add new head coordinates to front of body
-                eatGrow();
-            }
-            else {
-                // move as normal
-                snakeTrack.body.unshift(nextDown); // add new head coordinates to front of body
-                snakeTrack.body.pop(); // remove last element
-            }
-            
-        // Update snakeTrack.col from body's first element
-            snakeTrack.row = snakeTrack.body[0][0];
+    case "none":
+      // On startgame
+      return;
+  }
 
-            trackSnake();
-            break;
-        
-        case "pause":
-        // Pause direction
-        
-            break;
-        
-        case "none":
-            // On startgame
-            break;
-    }
-        
-        if (hitWall()) {
-            clearInterval(snakeGo);
-            alert("You have died!");
-            reset();
-            trackSnake();
-            return;
-        }
+  newTail = eatFood(newHead);
 
-    // snake.style.gridRow = snakeTrack.row;
-    // tail.style.gridRow = snakeTrack.rowTail;
+  if (newTail) {
+    // move forward
+    snakeTrack.body.unshift(newHead); // add new head coordinates to front of body
+    eatGrow();
+  } else {
+    // move as normal
+    snakeTrack.body.unshift(newHead); // add new head coordinates to front of body
+    snakeTrack.body.pop(); // remove last element
+  }
 
-    // snake.style.gridColumn = snakeTrack.col;
-    // tail.style.gridColumn = snakeTrack.colTail;
-}
+  // Update snakeTrack.col from body's first element
+  snakeTrack.row = snakeTrack.body[0][0];
+  snakeTrack.col = snakeTrack.body[0][1];
+
+  trackSnake();
+
+  if (hitWall()) {
+    clearInterval(snakeGo);
+    alert("You have died!");
+    reset();
+    trackSnake();
+    return;
+  }
+
+  // snake.style.gridRow = snakeTrack.row;
+  // tail.style.gridRow = snakeTrack.rowTail;
+
+  // snake.style.gridColumn = snakeTrack.col;
+  // tail.style.gridColumn = snakeTrack.colTail;
+};
 
 const reset = () => {
-    snakeTrack.row = 1;
-    snakeTrack.col = 1;
-    snakeTrack.body = [[1, 1], [1, 1], [1, 1], [1, 1]]
-    snake.forEach(x => {
-        x.style.gridRow = 1;
-        x.style.gridColumn = 1;
-    })
-    snakeTrack.direction = 'r';
-}
+  snakeTrack.row = 1;
+  snakeTrack.col = 1;
+  snakeTrack.body = [
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+  ];
+  snake.forEach((x) => {
+    x.style.gridRow = 1;
+    x.style.gridColumn = 1;
+  });
+  snakeTrack.direction = "r";
+};
 
 const pause = () => {
-    snakeTrack.direction = 'pause';
-    clearInterval(snakeGo);
-}
+  snakeTrack.direction = "pause";
+  clearInterval(snakeGo);
+};
 
 // =========== END GAME ===========  //
 
 const hitWall = () => {
-    // Check if hitting the wall;
-    let dead = false;
+  // Check if hitting the wall;
+  let dead = false;
 
-    if (snakeTrack.row > 20 || snakeTrack.row < 1 || snakeTrack.col > 20 || snakeTrack.col < 1) {
-        dead = true;
-    }
+  if (
+    snakeTrack.row > 20 ||
+    snakeTrack.row < 1 ||
+    snakeTrack.col > 20 ||
+    snakeTrack.col < 1
+  ) {
+    dead = true;
+  }
 
-    return dead;
-}
+  return dead;
+};
 
 // TO END GAME IF BUMPING INTO ITSELF
 const hitSelf = () => {
-    // Check if hitting itself;
-    let dead = false;
+  // Check if hitting itself;
+  let dead = false;
 
-    let coordinates = snakeTrack.body;
-    coordinates = coordinates.map(x => x.join("."));
+  let coordinates = snakeTrack.body;
+  coordinates = coordinates.map((x) => x.join("."));
 
-    
-    return dead;
-}
+  return dead;
+};
 
 // =========== ARROW KEYS EVENT ===========  //
 
 function checkKey(e) {
-    e = e || window.event;
-    e.preventDefault();
-    
-    switch (e.keyCode) {
-        case 37:
-            snakeTrack.direction = "l";
-            break;
-        case 38:
-            snakeTrack.direction = "u";
-            break;
-        case 39:
-            snakeTrack.direction = "r";
-            break;
-        case 40:
-            snakeTrack.direction = "d";
-            break;
-        case 32:
-            snakeTrack.direction = "pause";
-            break;
-    }
+  e = e || window.event;
+  e.preventDefault();
+
+  switch (e.keyCode) {
+    case 37:
+      snakeTrack.direction = "l";
+      break;
+    case 38:
+      snakeTrack.direction = "u";
+      break;
+    case 39:
+      snakeTrack.direction = "r";
+      break;
+    case 40:
+      snakeTrack.direction = "d";
+      break;
+    case 32:
+      snakeTrack.direction = "pause";
+      break;
+  }
 }
 
 const snakeGo = () => {
-        move();
-}
+  move();
+};
 
 playBtn.addEventListener("click", () => {
-    document.addEventListener("keydown", (e) => {
-        checkKey(e);
-    });
-    // PROBLEM - ON DOUBLE CLICK TIMER IS SET TWICE!!!
-    // Might be - because it sets the timer async 
+  document.addEventListener("keydown", (e) => {
+    checkKey(e);
+  });
+  // PROBLEM - ON DOUBLE CLICK TIMER IS SET TWICE!!!
+  // Might be - because it sets the timer async
 
+  clearInterval(snakeGo);
+  // TODO Interval shoudn't work on window.hidden
+  //     document.addEventListener('visibilitychange', function() {
+  //     if(document.hidden) {
+  //         // tab is now inactive
+  //         // temporarily clear timer using clearInterval() / clearTimeout()
+  //     }
+  //     else {
+  //         // tab is active again
+  //         // restart timers
+  //     }
+  // });
+
+  // Start game by setting interval timer
+
+  console.log(snakeTrack.direction);
+
+  if (snakeTrack.direction == "none") {
+    snakeTrack.direction = "r";
+    setInterval(snakeGo, 400);
+    console.log("this works");
+  } else {
     clearInterval(snakeGo);
-    // TODO Interval shoudn't work on window.hidden
-            //     document.addEventListener('visibilitychange', function() {
-            //     if(document.hidden) {
-            //         // tab is now inactive
-            //         // temporarily clear timer using clearInterval() / clearTimeout()
-            //     }
-            //     else {
-            //         // tab is active again
-            //         // restart timers
-            //     }
-            // });
-
-
-    // Start game by setting interval timer
-
-    console.log(snakeTrack.direction);
-
-    if (snakeTrack.direction == "none") {
-        snakeTrack.direction = 'r';
-        setInterval(snakeGo, 400);
-        console.log("this works");
-    }
-    else {
-        clearInterval(snakeGo);
-        reset();
-    }
-})
-
+    reset();
+  }
+});
 
 // =========== ADD FOOD TRACKING =========== //
 
 const food = document.getElementById("food");
 
-// FOOD // 
+// FOOD //
 
 const foodPosition = {
-    row: 7,
-    col: 8
-}
+  row: 7,
+  col: 8,
+};
 
 const moveFood = (row, col) => {
-    food.style.gridRow = row;
-    food.style.gridColumn = col;
-}
+  food.style.gridRow = row;
+  food.style.gridColumn = col;
+};
 
 const randomFood = () => {
+  let row = Math.round(Math.random() * 16);
+  let col = Math.round(Math.random() * 16);
 
-    let row = Math.round(Math.random() * 16);
-    let col = Math.round(Math.random() * 16);
-    
-    // TO AVOID PUTTING A SNACK ON SNAKE POSITION
-    // TODO UPDATE 
-    if (row == snakeTrack.row && col == snakeTrack.col) {
-        randomFood();
-    }
-    else {
-        foodPosition.row = row;
-        foodPosition.col = col;
-    
-        moveFood(row, col);
-    
-        return foodPosition;
-    }
+  // TO AVOID PUTTING A SNACK ON SNAKE POSITION
+  // TODO UPDATE
+  if (row == snakeTrack.row && col == snakeTrack.col) {
+    randomFood();
+  } else {
+    foodPosition.row = row;
+    foodPosition.col = col;
 
-} 
+    moveFood(row, col);
+
+    return foodPosition;
+  }
+};
 
 randomFood();
-
 
 // ========= SNAKE EATS SNACK ========= //
 
 const eatFood = (newMove) => {
-    // newMove [1, 4]
-    // Snack [1, 4]
+  // newMove [1, 4]
+  // Snack [1, 4]
 
-    if (newMove[0] == foodPosition.row && newMove[1] == foodPosition.col) {
-        randomFood();
-        console.log("yum");
-        const newTailCoor = snakeTrack.body[snakeLen - 1];
-        
-        console.log(newTailCoor);
-        console.log(snakeTrack.body);
+  if (newMove[0] == foodPosition.row && newMove[1] == foodPosition.col) {
+    randomFood();
+    console.log("yum");
+    const newTailCoor = snakeTrack.body[snakeLen - 1];
 
-        return newTailCoor;
-    }
-    return false;
+    console.log(newTailCoor);
+    console.log(snakeTrack.body);
 
-}
+    return newTailCoor;
+  }
+  return false;
+};
 
 const eatGrow = () => {
+  let newSegment = snakeSegment.content.cloneNode(true);
+  head.after(newSegment);
+  // gameBoard.prepend(newSegment);
+  snakeLen++;
 
-    let newSegment = snakeSegment.content.cloneNode(true);
-    head.after(newSegment);
-    // gameBoard.prepend(newSegment);
-    snakeLen++;
+  newSegment = gameBoard.querySelector(".snake-segment");
 
-    newSegment = gameBoard.querySelector(".snake-segment");
-    
-    let newTail = snakeTrack.body[snakeLen-1];
+  let newTail = snakeTrack.body[snakeLen - 1];
 
-    newSegment.style.gridRow = newTail[0];
-    newSegment.style.gridColumn = newTail[1];
-    
-    console.log("I'm big!");
-}
+  newSegment.style.gridRow = newTail[0];
+  newSegment.style.gridColumn = newTail[1];
+
+  console.log("I'm big!");
+};
